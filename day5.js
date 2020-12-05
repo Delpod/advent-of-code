2a0d40a75e883f1f6502e5fbebdfb4193c67ded9
Dawid Lubera <luberadawid@gmail.com>
2020-12-05 08:03:32 +0100

    Day 5

---
+++ day5.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const file = fs.readFileSync('./day5_input');
+const lines = file.toString().split('\n');
+
+const seatIds = [];
+
+for (let line of lines) {
+  let id = 0;
+  for (let i = 0; i < line.length; ++i) {
+    if (line[i] === 'B') {
+      id += 8 * 2 ** (6 - i);
+    } else if (line[i] === 'R') {
+      id += 2 ** (2 - (i - 7));
+    }
+  }
+
+  seatIds.push(id);
+}
+
+seatIds.sort((a, b) => a - b);
+
+console.log(seatIds[seatIds.length - 1]);
+console.log(seatIds.find((s, i, a) => s + 1 !== a[i + 1]) + 1);
