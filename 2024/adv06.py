4365f29d7ac4dcda1def56f0ba4aa6ef4db73f9e
Delpod <xvix56@gmail.com>
2024-12-06 22:09:21 +0100

    2024 - Day 6 minor optimizations

--- 2024/adv06.py
+++ 2024/adv06.py
@@ -2,17 +2,21 @@
 file = open('adv06.txt', 'r')
 lines = [l.strip() for l in file.readlines()]
 
+size = len(lines)
 x_start, y_start = None, None
-print_enabled = True
+print_enabled = False
 
-for i in range(len(lines)):
-    lines[i] = list(lines[i])
-    for j in range(len(lines[i])):
+for i in range(size):
+    line = []
+    for j in range(size):
         if lines[i][j] == "^":
             y_start, x_start = (i, j)
-            break
+        
+        line.append(lines[i][j] == '#')
+    lines[i] = line
 
-def check_guard_path(check_lines):
+
+def check_guard_path():
     x, y = x_start, y_start
     x_mov, y_mov = 0, -1
     distinct_tiles = set()
@@ -21,13 +25,16 @@ def check_guard_path(check_lines):
     while True:
         distinct_tiles.add((x, y))
 
-        new_x = x + x_mov
-        new_y = y + y_mov
+        x += x_mov
+        y += y_mov
         
-        if new_x < 0 or new_x >= len(check_lines[0]) or new_y < 0 or new_y >= len(check_lines):
+        if x < 0 or x >= size or y < 0 or y >= size:
             return True, len(distinct_tiles)
 
-        if check_lines[new_y][new_x] == "#":
+        if lines[y][x]:
+            x -= x_mov
+            y -= y_mov
+
             if x_mov != 0:
                 y_mov = x_mov
                 x_mov = 0
@@ -37,49 +44,45 @@ def check_guard_path(check_lines):
 
             move = (x, y, x_mov, y_mov)
 
-            if move not in moves:
-                moves[move] = True
-            else:
+            if move in moves:
                 return False, len(distinct_tiles)
 
+            moves[move] = True
             continue
 
-        x, y = new_x, new_y
-
         if print_enabled:
-            for i in range(len(check_lines) + 1):
+            for i in range(size + 1):
                 print("\r\033[A", end='')
 
-            for i in range(len(check_lines)):
+            for i in range(size):
                 line = ""
-                for j in range(len(check_lines[i])):
+                for j in range(size):
                     if (y, x) == (i, j):
                         line += "\033[92mO\033[00m"
                     elif (j, i) in distinct_tiles:
                         line += "\033[93mX\033[00m"
-                    elif check_lines[i][j] != ".":
-                        line += check_lines[i][j]
+                    elif lines[i][j]:
+                        line += '#'
                     else:
                         line += " " 
                 print(line)
-            print(f"x = {x}, y = {y}        ")
+            print(f"x = {x}, y = {y}    ")
 
 
 # Part 1
-_, result_p1 = check_guard_path(lines)
+_, result_p1 = check_guard_path()
 
 # Part 2
 result_p2 = 0
-for i in range(len(lines)):
-    for j in range(len(lines[i])):
-        if lines[i][j] == '.':
-            lines[i][j] = '#'
-            did_get_out, _ = check_guard_path(lines)
-            lines[i][j] = '.'
+for i in range(size):
+    for j in range(size):
+        if not lines[i][j]:
+            lines[i][j] = True
+            did_get_out, _ = check_guard_path()
+            lines[i][j] = False
             
             if not did_get_out:
                 result_p2 += 1
-
 
 print(f"Part 1: {result_p1}")
 print(f"Part 2: {result_p2}")
