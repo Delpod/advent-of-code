5bd10b74511a7cf3467345bae9154256d04b476f
Delpod <xvix56@gmail.com>
2024-12-23 21:55:16 +0100

    2024 - Day 17 Part 2

--- 2024/adv17.py
+++ 2024/adv17.py
@@ -1,5 +1,4 @@
 # Setup
-import math
 file = open('adv17.txt', 'r')
 lines: list[str] = file.read().splitlines()
 
@@ -7,72 +6,73 @@ O_A = int(lines[0].split(': ')[1])
 O_B = int(lines[1].split(': ')[1])
 O_C = int(lines[2].split(': ')[1])
 instructions = [int(x) for x in lines[4].split()[1].split(',')]
-i_count = len(instructions)
+ins_count = len(instructions)
 
 A, B, C = O_A, O_B, O_C
 
-def get_combo(value):
+def get_combo(value, a, b, c):
     if value == 4:
-        return A
+        return a
     if value == 5:
-        return B
+        return b
     if value == 6:
-        return C
+        return c
     if value == 7:
         raise ValueError("wild 7 appeared")
     else:
         return value
 
-def get_output():
-    global A, B, C
+def get_output(instructions, a, b, c):
     output = []
 
     index = 0
-    while index < i_count:
+    while index < ins_count:
         i = instructions[index]
 
         if i == 0:
-            A = math.floor(A / (2 ** get_combo(instructions[index + 1])))
+            a //= 2 ** get_combo(instructions[index + 1], a, b, c)
         elif i == 1:
-            B ^= instructions[index + 1]
+            b ^= instructions[index + 1]
         elif i == 2:
-            B = get_combo(instructions[index + 1]) % 8
+            b = get_combo(instructions[index + 1], a, b, c) % 8
         elif i == 3:
-            if A != 0:
+            if a != 0:
                 index = instructions[index + 1]
                 continue
         elif i == 4:
-            B ^= C
+            b ^= c
         elif i == 5:
-            output.append(get_combo(instructions[index + 1]) % 8)
+            output.append(get_combo(instructions[index + 1],  a, b, c) % 8)
         elif i == 6:
-            B = math.floor(A / (2 ** get_combo(instructions[index + 1])))
+            b = a // (2 ** get_combo(instructions[index + 1], a, b, c))
         elif i == 7:
-            C = math.floor(A / (2 ** get_combo(instructions[index + 1])))
+            c = a // (2 ** get_combo(instructions[index + 1], a, b, c))
 
         index += 2
     
     return output
 
-result_p1 = ','.join(map(str, get_output()))
+result_p1 = ','.join(map(str, get_output(instructions, A, B, C)))
 
-O_A = 706740000
-is_different = True
-while is_different:
-    A = O_A
-    if A % 10000 == 0:
-        print(A)
-    output = get_output()
-    is_different = len(output) != i_count
-    if not is_different:
-        for i, out in enumerate(output):
-            if instructions[i] != out:
-                is_different = True
-                break
-
-    if is_different:
-        O_A += 1
-result_p2 = O_A
+A = 0
+j = 1
+i_start = 0
+while j <= ins_count and j >= 0:
+    A <<= 3
+    for i in range(i_start, 8):
+        if instructions[-j:] == get_output(instructions, A + i, B, C):
+            break
+    else:
+        j -= 1
+        A >>= 3
+        i_start = A % 8 + 1
+        A >>= 3
+        continue
+    j += 1
+    A += i
+    i_start = 0
+    
+result_p2 = A
 
 print(f"Part 1: {result_p1}")
 print(f"Part 2: {result_p2}")
